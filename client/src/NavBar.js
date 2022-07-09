0e16d0afb2fe7e6a9cf66dca87a21fc38ef554d7
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-07-08 20:16:24 -0400

    logout re-added

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { UserContext } from './context/user'
+import Logout from './LogOutButton'
 
 export default function NavBar() {
 const { user, loggedIn } = useContext(UserContext)
@@ -12,6 +13,7 @@ const { user, loggedIn } = useContext(UserContext)
         <a><NavLink to="/my-groups" className="my-groups" user={user}>My Groups</NavLink></a>
         <a><NavLink to="/group-list" className="group-list" user={user}>List of Groups</NavLink></a>
         <a><NavLink to="/group-creator" className='group-creator' user={user}>Make a Group</NavLink></a>
+        <a><Logout /></a>
     </> : <>Please sign in to use the navigation menu</>}
     </nav> 
   )
